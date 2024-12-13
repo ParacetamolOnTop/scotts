95836419f7850ef64e0cee6be0bfee44eb34076f
Paracetamol <brioche.o.paracetamol@gmail.com>
2024-12-13 22:57:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
 
-// Open the sidebar when the toggle button is clicked
 toggleFiltersButton.addEventListener('click', () => {
     sidebar.classList.add('open');
     sidebar.classList.remove('hidden');
 });
 
-// Close the sidebar when the close button is clicked
 closeSidebarButton.addEventListener('click', () => {
     sidebar.classList.remove('open');
     sidebar.classList.add('hidden');
 });
 
 
-// Toggle the menu visibility when the menu toggle button is clicked
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle'); // Bouton toggle
+    const menu = document.querySelector('.menu'); // Menu à afficher/masquer
+  
+    // Ajouter un écouteur d'événement au bouton toggle
+    menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('open'); // Ajoute/Retire la classe "open" pour montrer/cacher le menu
+    });
+  });
+  
+  
